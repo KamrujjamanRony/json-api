fab068f06100e00bcac79c782ec1c42a95754b37
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-01-06 21:34:22 +0600

    add update and delete feature

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.get('/api/software', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/software/:id', (req, res) => {
+  const id = req.params.id;
+  const data = JSON.parse(fs.readFileSync('./software.json', 'utf-8'));
+  const record = data.find(record => record.id == id);
+
+  if (record) {
+    res.json(record);
+  } else {
+    res.status(404).json({ error: 'Record not found' });
+  }
+});
+
 app.post('/api/software', (req, res) => {
   const data = JSON.parse(fs.readFileSync('./software.json', 'utf-8'));
   const newRecord = req.body;
@@ -22,10 +34,39 @@ app.post('/api/software', (req, res) => {
   data.push(newRecord);
 
   fs.writeFileSync('./software.json', JSON.stringify(data, null, 2));
-  res.json(data);
+  res.json(newRecord);
 });
 
-// Implement other CRUD operations (PUT, DELETE) similarly
+app.put('/api/software/:id', (req, res) => {
+  const id = req.params.id;
+  const data = JSON.parse(fs.readFileSync('./software.json', 'utf-8'));
+  const updatedRecord = req.body;
+  updatedRecord.id = id;
+
+  const index = data.findIndex(record => record.id == id);
+
+  if (index !== -1) {
+    data[index] = updatedRecord;
+    fs.writeFileSync('./software.json', JSON.stringify(data, null, 2));
+    res.json(updatedRecord);
+  } else {
+    res.status(404).json({ error: 'Record not found' });
+  }
+});
+
+app.delete('/api/software/:id', (req, res) => {
+  const id = req.params.id;
+  const data = JSON.parse(fs.readFileSync('./software.json', 'utf-8'));
+  const index = data.findIndex(record => record.id == id);
+
+  if (index !== -1) {
+    const deletedRecord = data.splice(index, 1)[0];
+    fs.writeFileSync('./software.json', JSON.stringify(data, null, 2));
+    res.json(deletedRecord);
+  } else {
+    res.status(404).json({ error: 'Record not found' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
