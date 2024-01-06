e03a9a0c6d2f92e46e8e8f33b5e66478402c3302
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-01-06 19:12:13 +0600

    add get and post method

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("software.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-server.use(middlewares);
-server.use(router);
+const app = express();
+const port = 3000;
 
-server.listen(port);
+app.use(bodyParser.json());
+
+app.get('/api/software', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./software.json', 'utf-8'));
+  res.json(data);
+});
+
+app.post('/api/software', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./software.json', 'utf-8'));
+  const newRecord = req.body;
+  newRecord.id = generateUniqueId(); // Implement a function to generate a unique ID
+  data.push(newRecord);
+
+  fs.writeFileSync('./software.json', JSON.stringify(data, null, 2));
+  res.json(data);
+});
+
+// Implement other CRUD operations (PUT, DELETE) similarly
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+function generateUniqueId() {
+    return ((Math.random() * 9999999999 * Math.random() * 55555) - (Math.random() * 1111111111)).toString().split('.')[0];
+}
