687a30507bfa1563618111332c67a5aacdcfca83
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-01-08 12:22:42 +0600

    index.js fixed app.use(express.json());

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
-
 const app = express();
 const port = 3000;
-
 // Middleware
-app.use((req, res, next) => {
-  const origin = req.get('referer');
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  }
-  // Pass to next layer of middleware
-  if (req.method === 'OPTIONS') res.sendStatus(200);
-  else next();
-});
-app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 
 app.get('/api/software', (req, res) => {
   const data = JSON.parse(fs.readFileSync('./software.json', 'utf-8'));
@@ -80,10 +65,10 @@ app.delete('/api/software/:id', (req, res) => {
   }
 });
 
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
 function generateUniqueId() {
   return ((Math.random() * 9999999999 * Math.random() * 55555) - (Math.random() * 1111111111)).toString().split('.')[0];
 }
